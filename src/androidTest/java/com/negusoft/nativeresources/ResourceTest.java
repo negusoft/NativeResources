4cb1a90838dfc4551ff4a3cd360b7ce5487a722a
blurkidi <blurkidi@gmail.com>
2014-03-24 14:46:09 +0100

    Add anim and animator resource type access

--- src/androidTest/java/com/negusoft/nativeresources/ResourceTest.java
+++ src/androidTest/java/com/negusoft/nativeresources/ResourceTest.java
@@ -10,6 +10,8 @@ public class ResourceTest extends TestCase {
     private static final String SAMPLE_ID_NAME = "content";
     private static final String SAMPLE_STRING_NAME = "silent_mode";
     private static final String SAMPLE_DRAWABLE_NAME = "btn_check_buttonless_on";
+    private static final String SAMPLE_ANIM_NAME = "lock_screen_exit";
+    private static final String SAMPLE_ANIMATOR_NAME = "fragment_open_exit";
 
     public void testIdentifier() {
         int id = NativeResources.getIdentifier(SAMPLE_ID_NAME);
@@ -34,6 +36,16 @@ public class ResourceTest extends TestCase {
     public void testDrawable() {
         Drawable drawable = NativeResources.getDrawable(SAMPLE_DRAWABLE_NAME);
         Assert.assertNotNull(drawable);
+    }
+
+    public void testAnimIdentifier() {
+        int id = NativeResources.getAnimIdentifier(SAMPLE_ANIM_NAME);
+        Assert.assertTrue(id != 0);
+    }
+
+    public void testAnimatorIdentifier() {
+        int id = NativeResources.getAnimatorIdentifier(SAMPLE_ANIMATOR_NAME);
+        Assert.assertTrue(id != 0);
     }
 
 }
--- src/main/java/com/negusoft/nativeresources/NativeResources.java
+++ src/main/java/com/negusoft/nativeresources/NativeResources.java
@@ -29,6 +29,8 @@ public class NativeResources {
     private static final String TYPE_ID = "id";
     private static final String TYPE_STRING = "string";
     private static final String TYPE_DRAWABLE = "drawable";
+    private static final String TYPE_ANIM = "anim";
+    private static final String TYPE_ANIMATOR = "animator";
 
     /** Get a native identifier by name as in 'R.id.name'. */
     public static int getIdentifier(String name) {
@@ -61,5 +63,18 @@ public class NativeResources {
         int id = res.getIdentifier(name, TYPE_DRAWABLE, NATIVE_PACKAGE);
         return id == 0 ? null : res.getDrawable(id);
     }
+
+    /** Get a native id by name as in 'R.anim.name'. */
+    public static int getAnimIdentifier(String name) {
+        Resources res = Resources.getSystem();
+        return res.getIdentifier(name, TYPE_ANIM, NATIVE_PACKAGE);
+    }
+
+    /** Get a native id by name as in 'R.animator.name'. */
+    public static int getAnimatorIdentifier(String name) {
+        Resources res = Resources.getSystem();
+        return res.getIdentifier(name, TYPE_ANIMATOR, NATIVE_PACKAGE);
+    }
+
 
 }
